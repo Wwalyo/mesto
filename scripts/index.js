18d2711871409f27c20b5ef2e66ae425653eeede
Alena Goncharova <alenalesk@gmail.com>
2020-09-15 23:24:04 +0300

    вношу правки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,35 @@ console.log(`подключили скрипт`);
 
 const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.edit-button');
-const popupCloseButton = popup.querySelector('.close-popup-button');
+const popupCloseButton = popup.querySelector('.popup__close-button');
 const popupSaveButton = popup.querySelector('.content-form__save-button');
 const profile = document.querySelector('.profile-info');
 const popupDescription = popup.querySelector('.content-form__description');
+const formElement =  popup.querySelector('.content-form');
+const nameInput = formElement.querySelector('.content-form__input[name="name-input"]');
+const jobInput = formElement.querySelector('.content-form__input[name="description-input"]');
+const currentName = profile.querySelector('.profile-info__name');
+const currentJob = profile.querySelector('.profile-info__description');
+console.log(nameInput);
+console.log(jobInput);
 
 const popupOpen = (event) => {
   popup.classList.add('popup_is-opened');
+  nameInput.value = currentName.textContent;
+  jobInput.value = currentJob.textContent;
 };
 
 const popupClose = (event) => {
   popup.classList.remove('popup_is-opened');
 };
 
-
-popupOpenButton.addEventListener('click', popupOpen, false);
-popupCloseButton.addEventListener('click', popupClose, false);
-
-
-let formElement =  popup.querySelector('.content-form');
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    let nameInput = formElement.querySelector('.content-form__name')
-    let jobInput = formElement.querySelector('.content-form__description');
-    let name = nameInput.value;
-    let job = jobInput.value;
-    let currentName = profile.querySelector('.profile-info__name');
-    let currentJob = profile.querySelector('.profile-info__description');
-    currentName.textContent = name;
-    currentJob.textContent= job;
-    popup.classList.remove('popup_is-opened');
+function formSubmitHandler (event) {
+    event.preventDefault();
+    currentName.textContent = nameInput.value;
+    currentJob.textContent = jobInput.value;
+    popupClose();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', popupClose);
+popupOpenButton.addEventListener('click', popupOpen, false);
+popupCloseButton.addEventListener('click', popupClose, false);
