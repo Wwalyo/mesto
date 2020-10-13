2a00d522c7c30eb4616ad01369dcf3fa98662e00
Alena <alenalesk@gmail.com>
2020-10-13 13:13:00 +0300

    учтены замечания ревьюера

--- scripts/popups.js
+++ scripts/popups.js
@@ -3,7 +3,14 @@ const cardPopup = document.querySelector('.popup_type_new-card');
 const profilePopup = document.querySelector('.popup_type_profile');
 const imagePopup = document.querySelector('.image-popup');
 
+
 const openPopup = (modalWindow) => {
+  if (modalWindow.classList.contains('popup_type_new-card')) {
+    const formItem = modalWindow.querySelector('.content-form_type_new-card');
+    const submitbutton = formItem.querySelector('.content-form__save-button');
+    submitbutton.classList.add('content-form__save-button_disabled');
+    submitbutton.disabled = 'true';
+  }
   modalWindow.classList.add('popup_is-opened');
   document.addEventListener('keydown', closeESC);
 }
