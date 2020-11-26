2090da4b0dd725fa614d6b16f92b8dea2904eee4
Alena Goncharova <alenalesk@gmail.com>
2020-11-26 13:39:59 +0300

    что-то изменилось

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,7 +9,7 @@ export const profileEditButton = document.querySelector('.profile__edit-button')
 export const addCardButton = document.querySelector('.control-panel__add-button');
 export const editPhotoButton = document.querySelector('.profile__photo-overlay');
 export const cardTemplateSelector = '#card';
-export const cardsSelectors = '.cards';
+export const cardsSelector = '.cards';
 
 export const profileFormConfig = {
     formSelector: '.content-form_type_profile',
